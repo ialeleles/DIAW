b7b7cffecf5c8f985946af8f3438fbda400f849c
ialeleles <ialeleles@proton.me>
2026-08-20 17:29:27 -0300

    feat: adiciona get e delete com node.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require ('express')
+const app = express()
+
+const produtos = [
+    { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João" },
+        { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans" },
+        { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé" },
+        { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps" },
+        { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé" }
+]
+
+app.get ('/produtos', (req, res) => {
+    res.json(produtos)
+});
+
+app.delete ('/produtos/:id', (req, res) => {
+    const id = parseInt (req.params.id);
+    console.log(`excluir ${id}`)
+
+    const index = produtos.findIndex (produto => produto.id === id)
+
+    if(index != -1) {
+        produtos.splice (index, 1)
+        res.json (produtos)
+    } else {
+        res.status (404).json({Erro: 'Id não encontrado'});
+    }
+});
+
+app.listen(3000, (e) => {
+    console.log('Servidor ouvindo em http://localhost:3000')
+});
