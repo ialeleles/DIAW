f6d44beae0ef7dca92d2cf64cf1c9093537a789d
ialeleles <ialeleles@proton.me>
2026-09-01 17:40:03 -0300

    feat: adiciona formulário de cadastro de produto no html

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const produtos = [
   { "id": 5, "descricao": "Nescau 400gr", "preco": 8.00, "categoria": "Alimentos", "estoque": 6 }
 ];
 
-app.use('/site', express.static('site'));
+app.use(express.static('public'));
 
 app.get('/produtos', (req, res) => {
     res.json(produtos);
